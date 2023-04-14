b735d9d82ff08a43a5c244a7427bb081ed258469
Yuriy Butkevych <ybutkevych@reenbit.com>
2023-04-14 09:53:16 +0300

    Revert to a previous version

--- Fabillio.Inventory/Fabillio.Inventory.Cqrs/EventHandlers/UpdateProductsRemainingCount.cs
+++ Fabillio.Inventory/Fabillio.Inventory.Cqrs/EventHandlers/UpdateProductsRemainingCount.cs
@@ -13,41 +13,13 @@ using Raven.Client.Documents.Session;
 
 namespace Fabillio.Inventory.Cqrs.EventHandlers;
 
-// public class UpdateProductsRemainingCount: IHandle<OrderPlaced>
-// {
-//     private readonly IAsyncDocumentSession _documentSession;
-//
-//     public UpdateProductsRemainingCount(IAsyncDocumentSession documentSession)
-//     {
-//         _documentSession = documentSession;
-//     }
-//     
-//     public async Task Handle(OrderPlaced @event, CancellationToken cancellationToken)
-//     {
-//         var productIds = @event.Items.Select(x => x.ProductId).ToList();
-//         var products = await _documentSession.Query<Product>()
-//             .Where(x => x.ProductId.In(productIds))
-//             .ToListAsync(cancellationToken);
-//
-//         foreach (var product in products)
-//         {
-//             var placedCount = @event.Items.Where(x => x.ProductId == product.ProductId).Sum(x => x.Count);
-//             product.UpdateRemainingAmount(product.RemainingCount - placedCount);
-//         }
-//
-//         await _documentSession.SaveChangesAsync(cancellationToken);
-//     }
-// }
-
 public class UpdateProductsRemainingCount: IHandle<OrderPlaced>
 {
     private readonly IAsyncDocumentSession _documentSession;
-    private readonly IActorProxyFactory _actorProxyFactory;
 
-    public UpdateProductsRemainingCount(IAsyncDocumentSession documentSession, IActorProxyFactory actorProxyFactory)
+    public UpdateProductsRemainingCount(IAsyncDocumentSession documentSession)
     {
         _documentSession = documentSession;
-        _actorProxyFactory = actorProxyFactory;
     }
     
     public async Task Handle(OrderPlaced @event, CancellationToken cancellationToken)
@@ -59,14 +31,42 @@ public class UpdateProductsRemainingCount: IHandle<OrderPlaced>
 
         foreach (var product in products)
         {
-            var placedCount = @event.Items
-                .Where(x => x.ProductId == product.ProductId)
-                .Sum(x => x.Count);
-            
-            var actorId = new ActorId(product.ProductId.ToString());
-            var actorProxy = _actorProxyFactory.CreateActorProxy<IProductActor>(actorId, nameof(ProductActor));
-            
-            await actorProxy.BookProducts(placedCount);
+            var placedCount = @event.Items.Where(x => x.ProductId == product.ProductId).Sum(x => x.Count);
+            product.UpdateRemainingAmount(product.RemainingCount - placedCount);
         }
+
+        await _documentSession.SaveChangesAsync(cancellationToken);
     }
 }
+
+// public class UpdateProductsRemainingCount: IHandle<OrderPlaced>
+// {
+//     private readonly IAsyncDocumentSession _documentSession;
+//     private readonly IActorProxyFactory _actorProxyFactory;
+//
+//     public UpdateProductsRemainingCount(IAsyncDocumentSession documentSession, IActorProxyFactory actorProxyFactory)
+//     {
+//         _documentSession = documentSession;
+//         _actorProxyFactory = actorProxyFactory;
+//     }
+//     
+//     public async Task Handle(OrderPlaced @event, CancellationToken cancellationToken)
+//     {
+//         var productIds = @event.Items.Select(x => x.ProductId).ToList();
+//         var products = await _documentSession.Query<Product>()
+//             .Where(x => x.ProductId.In(productIds))
+//             .ToListAsync(cancellationToken);
+//
+//         foreach (var product in products)
+//         {
+//             var placedCount = @event.Items
+//                 .Where(x => x.ProductId == product.ProductId)
+//                 .Sum(x => x.Count);
+//             
+//             var actorId = new ActorId(product.ProductId.ToString());
+//             var actorProxy = _actorProxyFactory.CreateActorProxy<IProductActor>(actorId, nameof(ProductActor));
+//             
+//             await actorProxy.BookProducts(placedCount);
+//         }
+//     }
+// }
